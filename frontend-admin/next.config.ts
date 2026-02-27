b5368e78b35dab409600d37524679322bb7d07eb
Mahamadou Amadou Habou Gremah <mahamadou8877@gmail.com>
2026-02-27 15:42:08 +0100

    cleanning up repo for new project structure

--- frontend-admin/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-    output: 'standalone', // Pour Docker
-
-};
-
-export default nextConfig;
